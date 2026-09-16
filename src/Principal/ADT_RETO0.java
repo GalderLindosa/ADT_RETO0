d30546201e4c7494e4e680541eb43eb2405a23e5
ire220507-ctrl <ire220507@gmail.com>
2026-09-16 11:59:17 +0200

    Los paquetes y proyectos

--- src/Principal/ADT_RETO0.java
+++ src/Principal/ADT_RETO0.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package adt_reto0;
+package Principal;
+
+import vista.MainView;
 
 /**
  *
@@ -14,7 +16,8 @@ public class ADT_RETO0 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     MainView ui =new MainView();
+    ui.start();
     }
-    
-}
+    }
+
---
+++ src/vista/MainView.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+import utilidades.Utilidades;
+
+/**
+ *
+ * @author ire22
+ */
+public class MainView {
+     public int menu(){
+        int resp;
+        System.out.println("-1.Registrar producto"
+                + "\n-2.Registrar cliente"
+                + "\n-3.Consultar stock productos"
+                + "\n-4.Editar stock"
+                + "\n-5.Realizar pedido"
+                + "\n-6.Conusltar pedidos"
+                + "\n-7.Ver historial pedido productos"
+                + "\n-0.Salir");
+                
+        resp=Utilidades.leerInt(0, 7); 
+        return resp; 
+    }
+     
+     public void start(){
+         int option = -1;
+         while (option != 0){
+             option = this.menu();
+         }
+     }
+}
